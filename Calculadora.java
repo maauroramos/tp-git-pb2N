0e22799f98d883376ddffff385ba841656043ca0
Micaela Tatta <micaela.tatta@gmail.com>
2020-04-20 01:34:08 -0300

    modificaciones, no sigue operando

--- Calculadora.java
+++ Calculadora.java
@@ -1,29 +1,45 @@
-import java.util.Scanner;
-public class Calculadora {
- public static void main(String[] args) {
- Integer a;
- Integer b;
- Integer opcion;
- Integer resultado;
- Scanner teclado= new Scanner(System.in);
- do{
- System.out.println("Ingrese su operación /n 1  para dividir /n 2 para sumar /n 3 para restar /n 4para multiplicar");
- opcion = teclado.nextInt();
- }while(opcion <= 1 && opcion >= 4); 
- System.out.println("Ingrese el primer numero");
- a = teclado.nextInt();
- System.out.println("Ingrese el segundo numero");
- b = teclado.nextInt();
- if(opcion.equals(1)){
-	 resultado = a / b;
- }else if(opcion.equals(2)){
-	 resultado = a + b;
- }else if(opcion.equals(3)){
-	 resultado = a - b;
- }else{
-	 resultado = a * b;
- 
- }
- System.out.println("El resultado es " + resultado.toString());
- }
-}
+
+	import java.util.Scanner;
+
+	public class Calculadora {
+
+		public static void main(String[] args) {
+			Integer a;
+			Integer b;
+			Integer opcion;
+			Integer resultado=0;
+			Scanner teclado = new Scanner(System.in);
+			
+			
+		
+System.out.println("Ingrese su operacion n1 para multiplicar n2 para restar n3 para sumar n4 para dividir");
+				opcion = teclado.nextInt();
+			
+		
+		 if(opcion>=1&opcion<=4) {
+				System.out.println("Ingrese el primer numero");
+					a = teclado.nextInt();
+				System.out.println("Ingrese el segundo numero");
+					b = teclado.nextInt();
+			
+			switch(opcion){
+				
+				case 1: resultado = a * b;
+						break;
+				case 2: resultado = a - b;
+						break;
+				case 3: resultado = a + b;
+						break;
+				case 4: resultado = a / b;
+						break;
+				
+			}
+				System.out.println("El resultado es " +resultado);}
+	 	else
+	 {
+		 System.out.println("El número es incorrecto, no existe esa operación");
+	 }
+				
+		
+		
+			}}
